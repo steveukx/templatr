091c63b137733120a579d5146b17b401742058ec
Steve King <steve@server.local>
2011-11-14 22:50:39 +0000

    Allow for runat=server in script tags to prevent sending the script to the client

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 var fs = require('fs'),
 	 jsdom = require('jsdom'),
-	 xhr = require('xhrequest');
+	 xhr = require('xhrequest'),
+	 EventEmitter = require( "events" ).EventEmitter;
 
 /**
  * The Template is a convenience method for storing the content of a template and precaching any 
@@ -20,7 +21,7 @@ function Template(templateDir, templateName) {
 	this._initialiseScripts();
 }
 
-/** @type {String[]} */
+/** @type {Script[]} */
 Template.prototype._scripts = null;
 
 /**
@@ -31,76 +32,36 @@ Template.prototype._scripts = null;
  * directly from the DOM.
  */
 Template.prototype._initialiseScripts = function() {
-	var doc = jsdom.jsdom(this._template),
+	var doc = jsdom.jsdom(this._template, null, { features: {ProcessExternalResources : false} } ),
 		 scripts = doc.getElementsByTagName('script'),
 		 scriptContent = [];
 
 	for(var i = 0, l = scripts.length; i < l; i++) {
-		this._scripts[i] = false;
+		var script = this._scripts[i] = new Script(scripts[i], this._dir);
+		script.on('ready', this._onScriptLoaded.bind(this));
 
-		if(!scripts[i].src) {
-			this._readScriptTag(i, scripts[i]);
-		}
-		else if(!scripts[i].src.match(/^http/i)) {
-			this._readLocalScriptTag(i, scripts[i]);
-		}
-		else {
-			this._readRemoteScriptTag(i, scripts[i]);
+		if(script.serverOnly) {
+			scripts[i].parentNode.removeChild(scripts[i]);
 		}
 	}
+
+	this._template = doc.doctype + doc.innerHTML;
 };
 
 /**
- * Saves the content of a script tag at the given index in the array of scripts.
+ * Handler for when a Script tag fires the ready event.
  * 
- * @param {Number} index
- * @param {String} script
+ * @param {Script} script
+ * @param {String} scriptContent
  */
-Template.prototype._saveScriptContent = function(index, script) {
-
-console.log('adding ' + script.length + 'b to index ' + index);
-
-	this._scripts[index] = script;
-	if(this._scripts.indexOf(false) < 0) {
+Template.prototype._onScriptLoaded = function(script, scriptContent) {
+	if(this._scripts.every(function(scr) { return scr.loaded; })) {
 		this._ready = true;
 		console.log('Template ready for use...');
+//		console.log(this._template);
 	}
 };
 
-/**
- * Reads the content of an inline script tag.
- * 
- * @param {Number} index
- * @param {Element} tag
- */
-Template.prototype._readScriptTag = function(index, tag) {
-	this._saveScriptContent(index, tag.firstChild.nodeValue);
-};
-
-/**
- * Reads the content of a relative script tag - assumes that the source of the script tag is relative
- * to the root of the website and that the root of the website is also the directory root for this template.
- * 
- * @param {Number} index
- * @param {Element} tag
- */
-Template.prototype._readLocalScriptTag = function(index, tag) {
-//	this._saveScriptContent(index, fs.readFileSync(this._dir + tag.src, 'utf8'));
-	fs.readFile(this._dir + tag.src, 'utf8', (function(err, data) { this._saveScriptContent(index, data); }).bind(this) );
-};
-
-/**
- * Reads the content of an script tag from any remote server.
- * 
- * @param {Number} index
- * @param {Element} tag
- */
-Template.prototype._readRemoteScriptTag = function(index, tag) {
-	xhr(tag.src, {
-		success: this._saveScriptContent.bind(this, index),
-		error: this._saveScriptContent.bind(this, index, '')
-	});
-};
 
 /**
  * Sets up an express / connect compatible middleware for creating the template and sending it out on a
@@ -123,6 +84,108 @@ Template.prototype._middleware = function(req, res, next) {
 			res.send( win.document.doctype + win.document.innerHTML);
 		}
 	});
+};
+
+
+/**
+ * The Script class wraps a SCRIPT element in the template document and is responsible for exposing
+ * content of the script tag to the template.
+ *
+ * @param {Element} node
+ * @param {String} srcPath
+ * @extends {EventEmitter}
+ */
+function Script(node, srcPath) {
+
+	EventEmitter.call(this);
+
+	this.serverOnly = !!(node.getAttribute('runat') || '').match(/server/i);
+	if(!node.src) {
+		Script.readInlineScriptTag(this, node);
+	}
+	else {
+		this.src = node.src;
+		if(!this.src.match(/^http/)) {
+			Script.readLocalScriptFile(this, srcPath + this.src);
+		}
+		else {
+			Script.readRemoteScriptFile(this, this.src);
+		}
+	}
+}
+Script.prototype = Object.create( EventEmitter.prototype );
+
+/** @type {Boolean} */
+Script.prototype.loaded = false;
+
+/** @type {String} */
+Script.prototype.content = '';
+
+/** @type {Boolean} */
+Script.prototype.serverOnly = false;
+
+/** @type {String} */
+Script.prototype.src = '';
+
+/**
+ * Reads the content of an inline script tag and on next tick pushes it into the Script instance
+ * 
+ * @param {Script} script
+ * @param {Element} node
+ */
+Script.readInlineScriptTag = function(script, node) {
+	setTimeout(function() {
+		script.setContent( node.firstChild && node.firstChild.nodeValue );
+	},1);
+};
+
+/**
+ * Reads the content of a relative script tag - assumes that the source of the script tag is relative
+ * to the root of the website and that the root of the website is also the directory root for this template.
+ * 
+ * @param {Script} script
+ * @param {String} src The base path for any relative URL
+ */
+Script.readLocalScriptFile = function(script, src) {
+	setTimeout(function() {
+		script.setContent(fs.readFileSync(src, 'utf8'));
+	},1);
+};
+
+/**
+ * Reads the content of an script tag from any remote server.
+ * 
+ * @param {Script} script
+ * @param {String} src The absolute URL for the script (currently must be either http or https)
+ */
+Script.readRemoteScriptFile = function(script, src) {
+	xhr(src, {
+		success: script.setContent.bind(script),
+		error: script.setContent.bind(script, '')
+	});
+};
+
+/**
+ * Sets the contnet for the script tag to the supplied JavaScript contnet.
+ * 
+ * @param {String} content
+ * @return {Script} this
+ */
+Script.prototype.setContent = function(content) {
+	this.content = content;
+	this.loaded = true;
+
+	this.emit('ready', this, this.content);
+	return this;
+};
+
+/**
+ * Override default toString behaviour to return the actual content of the SCRIPT tag to allow
+ * for easily concatenating an array of String instances into a single javascript file.
+ * @return {String}
+ */
+Script.prototype.toString = function() {
+	return this.content;
 };
 
 module.exports = Template;
